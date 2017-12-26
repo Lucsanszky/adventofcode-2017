43d317cc415420dabc58538bf075ea4eab3ab258
Lucsanszky <dan.lucsanszky@gmail.com>
2017-12-26 01:48:38 +0100

    updates code to johan-tibell style

--- 1.hs
+++ 1.hs
@@ -7,49 +7,38 @@ script
 -}
 
 import           Data.List    (group)
-import           Network.Curl (CurlOption (CurlCookieFile), curlGetString)
+import           Network.Curl
+    ( CurlOption (CurlCookieFile)
+    , curlGetString
+    )
 
-parseInts
-    :: String
-    -> [Int]
-parseInts
-    = map (\x -> read [x] :: Int)
+parseInts :: String -> [Int]
+parseInts = map (\x -> read [x] :: Int)
 
-captcha
-    :: [Int]
-    -> Int
-captcha xs
-    = foldr (\x -> (+) (head x * (length x - 1))) 0
-    . group
-    $ xs ++ [head xs]
+captcha :: [Int] -> Int
+captcha xs =
+    foldr (\x -> (+) (head x * (length x - 1))) 0 . group $
+    xs ++ [head xs]
 
-altCaptcha
-    :: [Int]
-    -> Int
-altCaptcha xs
-    = foldr
-    (\ (x1, x2) ->
-       if x1 == x2
-          then (+) (x1 + x2)
-          else (+) 0
-    ) 0 (zip xs1 xs2)
+altCaptcha :: [Int] -> Int
+altCaptcha xs =
+    foldr
+        (\(x1, x2) ->
+              if x1 == x2
+                  then (+) (x1 + x2)
+                  else (+) 0)
+        0
+        (zip xs1 xs2)
   where
-    (xs1, xs2)
-        = (take half xs, drop half xs)
-    half
-        = length xs `div` 2
+    (xs1, xs2) = (take half xs, drop half xs)
+    half = length xs `div` 2
 
-main
-    :: IO ()
-main
-    = do
-      (_, s) <- curlGetString
-                "http://adventofcode.com/2017/day/1/input"
-                [CurlCookieFile ".cookie.txt"]
-
-      mapM_ (\ f ->
-               print
-             . f
-             . parseInts
-             $ takeWhile (/= '\n') s
-             ) [captcha, altCaptcha]
+main :: IO ()
+main = do
+    (_, s) <-
+        curlGetString
+            "http://adventofcode.com/2017/day/1/input"
+            [CurlCookieFile ".cookie.txt"]
+    mapM_
+        (\f -> print . f . parseInts $ takeWhile (/= '\n') s)
+        [captcha, altCaptcha]
--- 2.hs
+++ 2.hs
@@ -9,53 +9,37 @@
 import           Control.Arrow   ((&&&))
 import           Data.List       (sortBy)
 import           Data.List.Split (splitOn)
-import           Network.Curl    (CurlOption (CurlCookieFile), curlGetString)
+import           Network.Curl
+    ( CurlOption (CurlCookieFile)
+    , curlGetString
+    )
 
-parseInts
-    :: [String]
-    -> [Int]
-parseInts
-    = map (\ x -> read x :: Int)
+parseInts :: [String] -> [Int]
+parseInts = map (\x -> read x :: Int)
 
-checksum
-    :: [[Int]]
-    -> Int
-checksum
-    = foldr (\ xs -> (+) (maximum xs - minimum xs)) 0
+checksum :: [[Int]] -> Int
+checksum = foldr (\xs -> (+) (maximum xs - minimum xs)) 0
 
-altChecksum
-    :: [[Int]]
-    -> Int
-altChecksum
-    = foldr (\ xs -> (+) (findDiv (sorted xs) [])) 0
+altChecksum :: [[Int]] -> Int
+altChecksum = foldr (\xs -> (+) (findDiv (sorted xs) [])) 0
   where
-    sorted
-        = sortBy (flip compare)
-    findDiv
-        :: [Int]
-        -> [Int]
-        -> Int
-    findDiv (x0 : x1 : xs) ys
-        | x0 `mod` x1 == 0
-        = x0 `div` x1
-        | otherwise
-        = findDiv (x0 : xs) (ys ++ [x1])
-    findDiv _ ys
-        = findDiv ys []
+    sorted = sortBy (flip compare)
+    findDiv :: [Int] -> [Int] -> Int
+    findDiv (x0:x1:xs) ys
+        | x0 `mod` x1 == 0 = x0 `div` x1
+        | otherwise = findDiv (x0 : xs) (ys ++ [x1])
+    findDiv _ ys = findDiv ys []
 
-main
-    :: IO ()
-main
-    = do
-      (_, s) <- curlGetString
-                "http://adventofcode.com/2017/day/2/input"
-                [CurlCookieFile ".cookie.txt"]
-
-      mapM_
-          (\ f ->
-             print
-           . f
-           . map (parseInts . words)
-           . takeWhile (/= "")
-           $ splitOn "\n" s
-          ) [checksum, altChecksum]
+main :: IO ()
+main = do
+    (_, s) <-
+        curlGetString
+            "http://adventofcode.com/2017/day/2/input"
+            [CurlCookieFile ".cookie.txt"]
+    mapM_
+        (\f ->
+              print .
+              f .
+              map (parseInts . words) . takeWhile (/= "") $
+              splitOn "\n" s)
+        [checksum, altChecksum]
